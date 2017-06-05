e0699c649d946361d69699cc9a6dac8310fbbfc6
Trent Russi <trussi@gmail.com>
2017-06-04 22:54:34 -0700

    Problem 1 and 2

---
+++ util.js
@@ -0,0 +1,17 @@
+function is_prime(n) {
+    if (n <= 1) {
+        return false;
+    } else if (n <= 3) {
+        return true;
+    } else if (n % 2 == 0 || n % 3 == 0) {
+        return false;
+    }
+    var i = 5;
+    while (i * i <= n) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return false;
+        }
+        i += 6;
+    }
+    return true;
+}
